118a6ade71727a8c19614c051b85148f01479b57
Cody Lanier <codylanier09@gmail.com>
2020-05-26 09:40:39 -0700

    initial commit

---
+++ DTT_Update_1-0_S1AL.sql
@@ -0,0 +1,8 @@
+-- GameData1
+-- Author: PC
+-- DateCreated: 7/1/2018 8:21:42 PM
+--------------------------------------------------------------
+UPDATE TurnSegments SET TimeLimit_Base=65 , TimeLimit_PerCity=4 , TimeLimit_PerUnit=1 WHERE TurnSegmentType='TURNSEG_SINGLEPHASE';
+UPDATE TurnSegments SET TimeLimit_Base=120 WHERE TurnSegmentType='TURNSEG_WORLDCONGRESS_1';
+UPDATE TurnSegments SET TimeLimit_Base=120 WHERE TurnSegmentType='TURNSEG_WORLDCONGRESS_2';
+UPDATE TurnSegments SET TimeLimit_Base=120 WHERE TurnSegmentType='TURNSEG_WORLDCONGRESS_RESOLUTION';
